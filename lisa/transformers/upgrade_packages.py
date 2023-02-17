addab3a1c4f7c707ae9807de1e8a25c3a9f91218
Kameron Carr <kameroncarr@microsoft.com>
2023-02-17 11:45:38 -0800

    Separate unattended-updates from full update
    There is a difference in behavior between unattended-upgrade and running apt upgrade. For this reason, I am switching this back to using unattended upgrade.
    Since unattended-upgrade doesn't always upgrade all packages, I need to remove the packages that don't get upgraded from the return value.

--- lisa/transformers/upgrade_packages.py
+++ lisa/transformers/upgrade_packages.py
@@ -45,6 +45,10 @@ class UpgradeInstaller(subclasses.BaseClassWithRunbookMixin):
         self._node = node
         self._log = get_logger("upgrade_installer", parent=parent_log)
 
+    @classmethod
+    def type_schema(cls) -> Type[schema.TypedSchema]:
+        return UpgradeInstallerSchema
+
     def validate(self) -> None:
         raise NotImplementedError()
 
@@ -102,9 +106,128 @@ class UnattendedUpgradeInstaller(UpgradeInstaller):
     def type_name(cls) -> str:
         return "unattended_upgrade"
 
+    def validate(self) -> None:
+        assert isinstance(self._node.os, Debian), (
+            f"The '{self.type_name()}' installer only supports Debian family. "
+            f"The current os is {self._node.os.name}"
+        )
+
+    def install(self) -> List[str]:
+        if self.runbook.repo_url.strip():
+            self._update_repo()
+
+        return self._update_packages()
+
+    def _update_repo(self) -> None:
+        node: Node = self._node
+        runbook: UpgradeInstallerSchema = self.runbook
+        repo_url = runbook.repo_url
+        assert isinstance(node.os, Ubuntu)
+        release = node.os.information.codename
+
+        assert (
+            release
+        ), f"cannot find codename from the os version: {node.os.information}"
+
+        sed = node.tools[Sed]
+        sed.run(
+            f"-E '/{release} main/!s/(.*)/#\\1/' -i /etc/apt/sources.list",
+            shell=True,
+            sudo=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to comment out other repo sources",
+        )
+
+        if runbook.proposed:
+            version_name = f"{release}-proposed"
+        else:
+            version_name = release
+
+        repo_entry = (
+            f"deb {repo_url} {version_name} " f"restricted main multiverse universe"
+        )
+        node.os.add_repository(repo_entry)
+
+    def _update_packages(self) -> List[str]:
+        node: Node = self._node
+        assert isinstance(node.os, Debian)
+
+        # Make sure unattended-upgrade is installed
+        cmd_result = node.execute(
+            "which unattended-upgrade",
+            sudo=True,
+            shell=True,
+        )
+        if 0 != cmd_result.exit_code:
+            node.os.install_packages("unattended-upgrades")
+        if type(node.os) == Debian:
+            if node.os.information.version >= "10.0.0":
+                node.execute(
+                    "mkdir -p /var/cache/apt/archives/partial",
+                    sudo=True,
+                    shell=True,
+                    expected_exit_code=0,
+                    expected_exit_code_failure_message=(
+                        "fail to make folder /var/cache/apt/archives/partial"
+                    ),
+                )
+            else:
+                node.os.install_packages(["debian-keyring", "debian-archive-keyring"])
+
+        node.execute(
+            "apt update",
+            sudo=True,
+            shell=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to run apt-update",
+        )
+        result = node.execute(
+            "apt list --upgradable | awk '{printf(\"%s %s\\n\",$1,$2)}'",
+            sudo=True,
+            shell=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to get upgrade-package list",
+        )
+        upgradable_before = [package.strip() for package in result.stdout.split("\n")]
+
+        node.execute(
+            "unattended-upgrade -d -v",
+            sudo=True,
+            shell=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to run unattended-upgrade",
+            timeout=2400,
+        )
+
+        result = node.execute(
+            "apt list --upgradable | awk '{printf(\"%s %s\\n\",$1,$2)}'",
+            sudo=True,
+            shell=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to get upgrade-package list",
+        )
+        upgradable_after = [package.strip() for package in result.stdout.split("\n")]
+
+        # Return a list packages that were upgraded
+        return [
+            package for package in upgradable_before if package not in upgradable_after
+        ]
+
+
+class FullUpgradeInstaller(UpgradeInstaller):
+    def __init__(
+        self,
+        runbook: Any,
+        node: Node,
+        parent_log: Logger,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(runbook, node, parent_log, *args, **kwargs)
+
     @classmethod
-    def type_schema(cls) -> Type[schema.TypedSchema]:
-        return UpgradeInstallerSchema
+    def type_name(cls) -> str:
+        return "full_upgrade"
 
     def validate(self) -> None:
         assert isinstance(self._node.os, Debian), (
@@ -166,11 +289,20 @@ class UnattendedUpgradeInstaller(UpgradeInstaller):
             expected_exit_code=0,
             expected_exit_code_failure_message="fail to get upgrade-package list",
         )
-        node.os.update_packages("")
+        upgradable_before = [package.strip() for package in result.stdout.split("\n")]
 
-        # Some of the beginning output is not packages eg. "Warning..." "Listing..."
-        packages = result.stdout.split("\n")
-        while len(packages[0].split()) != 2:
-            packages = packages[1:]
+        node.os.update_packages("--with-new-pkgs")
 
-        return packages
+        result = node.execute(
+            "apt list --upgradable | awk '{printf(\"%s %s\\n\",$1,$2)}'",
+            sudo=True,
+            shell=True,
+            expected_exit_code=0,
+            expected_exit_code_failure_message="fail to get upgrade-package list",
+        )
+        upgradable_after = [package.strip() for package in result.stdout.split("\n")]
+
+        # Return a list packages that were upgraded
+        return [
+            package for package in upgradable_before if package not in upgradable_after
+        ]
