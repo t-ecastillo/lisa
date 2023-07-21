aedf0cef52b7663e366403d5c2f1d0f14906ca24
adityanagesh <adityanagesh@microsoft.com>
2023-07-21 08:36:16 +0530

    Add Transformer EXPANDED_CLEANUP
    EXPANDED_CLEANUP is used to run transformers
    after the Transfromer EXPANDED phase.

--- lisa/sut_orchestrator/azure/transformers.py
+++ lisa/sut_orchestrator/azure/transformers.py
@@ -303,7 +303,6 @@ class DeployTransformer(Transformer):
         platform.deploy_environment(environment)
 
         resource_group_name = get_environment_context(environment).resource_group_name
-
         # generate return results
         results = {
             self.__resource_group_name: resource_group_name,
